0c906cf4dde5c44efb5402f25672a5afaabe1d06
Corey Benefrayim <58489646+coreymbe@users.noreply.github.com>
2020-05-14 16:16:58 -0700

    Create init.pp

---
+++ manifests/init.pp
@@ -0,0 +1,12 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include nodripa
+
+class nodripa (
+  String $ssh_key
+){
+  include nodripa::install
+}
