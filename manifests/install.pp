1b0e2ed32485c2e7ced3455ad0c9a442f92d9cf3
Corey Benefrayim <58489646+coreymbe@users.noreply.github.com>
2020-05-14 16:20:42 -0700

    Create install.pp

---
+++ manifests/install.pp
@@ -0,0 +1,46 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include nodripa::install
+
+class nodripa::install {
+
+  file { '/tmp/puppet-bolt.rpm':
+    source => ["puppet:///modules/nodripa/puppet-bolt.rpm"],
+    ensure => present,
+    before => Package['puppet-bolt'],
+  }
+  package { 'puppet-bolt':
+    ensure => 'installed',
+    provider => rpm,
+    source => ["/tmp/puppet-bolt.rpm"],
+  }
+  file {'/tmp/certname-replace.sh':
+    ensure => present,
+    source => ["puppet:///modules/nodripa/certname-replace.sh"],
+    mode => '0744',
+    before => Service['nodripa'],
+  }
+  file {'/tmp/bolt-nodripa.sh':
+    ensure => present,
+    source => ["puppet:///modules/nodripa/bolt-nodripa.sh"],
+    mode => '0744',
+    before => Service['nodripa'],
+  }
+  file { '/root/.ssh/nodripa_rsa':
+    ensure => present,
+    content => $nodripa::ssh_key,
+    mode => '0600',
+  }
+  file { '/etc/systemd/system/nodripa.service':
+    ensure => present,
+    source => ["puppet:///modules/nodripa/nodripa.service"],
+    notify => Service['nodripa'],
+  }
+  service { 'nodripa':
+    enable => true,
+    ensure => running,
+  }
+}
