53a9a9b779b6ed59d969ca4af0b6cff2bead7554
Corey Benefrayim <58489646+coreymbe@users.noreply.github.com>
2020-05-19 15:14:36 -0700

    Delete bolt-nodripa.sh

--- files/bolt-nodripa.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mom_certname=$(puppet config print server --section main)
-/opt/puppetlabs/bin/bolt command run '/opt/puppetlabs/bin/puppet node purge localcert' --targets $mom_certname --no-host-key-check --private-key /root/.ssh/nodripa_rsa
