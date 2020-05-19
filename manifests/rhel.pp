9fa58ee20bd0afb0cb12472c87f83d5df9894ed9
Corey Benefrayim <58489646+coreymbe@users.noreply.github.com>
2020-05-19 15:18:58 -0700

    Update rhel.pp

--- manifests/rhel.pp
+++ manifests/rhel.pp
@@ -1,48 +1,44 @@
-# @summary A short summary of the purpose of this class
-#
-# A description of what this class does
-#
 # @example
-#   include nodripa::rhel
+#   include nodripa::install
 
 class nodripa::rhel {
 
   yumrepo { 'puppet-tools':
-    name => "puppet-tools",
-    ensure => present,
-    baseurl => "http://yum.puppet.com/puppet-tools/el/${facts['os']['release']['major']}/\$basearch",
-    enabled => '1',
-    gpgcheck => '0',
-    before => Package['puppet-bolt'],
+    ensure         => present,
+    name           => 'puppet-tools',
+    baseurl        => "http://yum.puppet.com/puppet-tools/el/${facts['os']['release']['major']}/\$basearch",
+    enabled        => '1',
+    gpgcheck       => '0',
+    before         => Package['puppet-bolt'],
   }
   package { 'puppet-bolt':
-    ensure => 'installed',
-    provider => yum,
+    ensure         => 'installed',
+    provider       => yum,
   }
   file {'/tmp/certname-replace.sh':
-    ensure => present,
-    source => ["puppet:///modules/nodripa/rhel-certname-replace.sh"],
-    mode => '0744',
-    before => Service['nodripa'],
+    ensure         => present,
+    source         => ['puppet:///modules/nodripa/certname-replace.sh'],
+    mode           => '0744',
+    before         => Service['nodripa'],
   }
   file {'/tmp/bolt-nodripa.sh':
-    ensure => present,
-    source => ["puppet:///modules/nodripa/bolt-nodripa.sh"],
-    mode => '0744',
-    before => Service['nodripa'],
+    ensure         => present,
+    content        => epp('nodripa/bolt-nodripa.sh.epp'),
+    mode           => '0744',
+    before         => Service['nodripa'],
   }
-  file { '/root/.ssh/nodripa_rsa':
-    ensure => present,
-    content => $nodripa::ssh_key,
-    mode => '0600',
+  file { $nodripa::ssh_key:
+    ensure         => present,
+    content        => $nodripa::private_key,
+    mode           => '0600',
   }
   file { '/etc/systemd/system/nodripa.service':
-    ensure => present,
-    source => ["puppet:///modules/nodripa/nodripa.service"],
-    notify => Service['nodripa'],
+    ensure         => present,
+    source         => ['puppet:///modules/nodripa/nodripa.service'],
+    notify         => Service['nodripa'],
   }
   service { 'nodripa':
-    enable => true,
-    ensure => running,
+    ensure         => running,
+    enable         => true,
   }
 }
