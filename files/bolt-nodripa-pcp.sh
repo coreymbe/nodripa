512cf941d774d4147773eb53bece8597af72380c
Corey Benefrayim <58489646+coreymbe@users.noreply.github.com>
2020-06-25 15:08:32 -0700

    Update and rename files/bolt-nodripa-pcp.sh to templates/bolt-nodripa-pcp.sh.epp

--- files/bolt-nodripa-pcp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mom_certname=$(/opt/puppetlabs/bin/puppet config print server --section main)
-bolt command run '/opt/puppetlabs/bin/puppet node purge localcert' --targets $mom_certname
